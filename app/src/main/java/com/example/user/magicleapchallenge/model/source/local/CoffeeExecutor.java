0c2de9fe865754996a51771fda2f901b6b88eb0d
Manroop Singh <singh@manroop.com>
2018-05-31 19:25:11 -0400

    create coffeeExecutor class with callback

--- app/src/main/java/com/example/user/magicleapchallenge/model/source/local/CoffeeExecutor.java
+++ app/src/main/java/com/example/user/magicleapchallenge/model/source/local/CoffeeExecutor.java
@@ -1,6 +1,41 @@
 package com.example.user.magicleapchallenge.model.source.local;
 
-public class CoffeeExecutor {
+import android.os.AsyncTask;
 
-    // TODO: 5/25/18 Extract asynchronous tasks from LocalDataSource
+public class CoffeeExecutor extends AsyncTask<CoffeeExecutor.TaskRunner, Integer, Object> {
+
+
+    TaskRunner taskRunner;
+
+    public  void execute(TaskRunner taskRunner) {
+
+        this.taskRunner = taskRunner;
+        CoffeeExecutor coffeeExecutor = new CoffeeExecutor();
+        coffeeExecutor.execute();
+
+    }
+
+    @Override
+    protected Object doInBackground(TaskRunner... taskRunners) {
+        taskRunners[0].runOnWorker();
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+    }
+
+    interface TaskRunner {
+        void runOnWorker();
+
+        void onSuccess(Object... objects);
+
+        void onFailure(String error);
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/user/magicleapchallenge/model/source/local/TaskExecutor.java
@@ -0,0 +1,22 @@
+package com.example.user.magicleapchallenge.model.source.local;
+
+import android.os.AsyncTask;
+
+public class TaskExecutor extends AsyncTask<String, Integer, String> {
+
+
+    public static void execute() {
+
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+
+        super.onPostExecute(s);
+    }
+}
